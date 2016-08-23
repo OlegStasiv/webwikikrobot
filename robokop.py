edd5f37338aac0d2154c5831e8349e74800e15a5
olegstasiv <oleg.stasiv@hotmail.com>
2016-08-23 16:46:42 +0300

    18

--- robokop.py
+++ robokop.py
@@ -11,12 +11,11 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from wikiapi import WikiApi
 
-
 urllib3.disable_warnings()
 logging.captureWarnings(True)
 
 wiki = WikiApi()
-wiki = WikiApi({ 'locale' : 'uk'})
+wiki = WikiApi({'locale': 'uk'})
 results = wiki.find('ThinkMObiles')
 article = wiki.get_article(results[0])
 # article.summary
@@ -24,47 +23,35 @@ text_file = open("New.txt", "w")
 text_file.write((article.content).encode('utf-8'))
 text_file.close()
 
-#print article.content
+# print article.content
 
 test1filehandle = open('Old.txt', 'r')
-test2filehandle=open('New.txt', 'r')
-test3filehandle=open('some_output_file.txt', 'w')
-test1list= test1filehandle.readlines()
-test2list=test2filehandle.readlines()
-k=1
-for i,j in zip(test1list,test2list):
-    if i !=j:
-        test3filehandle.write("Line Number:" +str(k)+' ')
-        test3filehandle.write(i.rstrip("\n") + ' '+ j)
-    k=int(k)
-    k=k+1
+test2filehandle = open('New.txt', 'r')
+test3filehandle = open('some_output_file.txt', 'w')
+test1list = test1filehandle.readlines()
+test2list = test2filehandle.readlines()
+k = 1
+for i, j in zip(test1list, test2list):
+    if i != j:
+        test3filehandle.write("Line Number:" + str(k) + ' ')
+        test3filehandle.write(i.rstrip("\n") + ' ' + j)
+    k = int(k)
+    k = k + 1
 test3filehandle.close()
 
 try:
     if os.stat('some_output_file.txt').st_size > 0:
-       print "All good"
+        print "All good"
 
-       driver = webdriver.Remote(
-           command_executor='http://localhost:4444/wd/hub',
-           desired_capabilities={
-               "browserName": "firefox",
-               "version": "45"
-           })
-
-       driver.implicitly_wait(30)
-
-
-       #driver = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities=options.to_capabilities())
-       driver = WebDriver("http://localhost:4444/wd/hub", "firefox", "ANY")
-       #driver = webdriver.Firefox()
-       driver.get("https://uk.wikipedia.org/wiki/ThinkMobiles")
-       assert "ThinkMobiles — Вікіпедія" in driver.title
-       # elem = driver.find_element_by_name("q")
-       # elem.send_keys("pycon")
-       # elem.send_keys(Keys.RETURN)
-       # assert "No results found." not in driver.page_source
-       driver.close()
+        driver = webdriver.Chrome()
+        driver.get("https://uk.wikipedia.org/wiki/ThinkMobiles")
+        assert "ThinkMobiles — Вікіпедія" in driver.title
+        # elem = driver.find_element_by_name("q")
+        # elem.send_keys("pycon")
+        # elem.send_keys(Keys.RETURN)
+        # assert "No results found." not in driver.page_source
+        driver.close()
     else:
-       print "empty file"
+        print "empty file"
 except OSError:
     print "No file"
